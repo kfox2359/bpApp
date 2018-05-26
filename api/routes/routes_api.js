2734f1ece2872cf6402ecbb1b659bc9a1bf3bbed
Kevin Fox <kfox2359@gmail.com>
2018-05-26 18:07:59 -0500

    Small change to endpoint and a new welcome screen

--- api/routes/routes_api.js
+++ api/routes/routes_api.js
@@ -4,7 +4,7 @@ var db = require('../modules/db.js');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+    res.sendFile("./routesInfo.html",{root:__dirname});
 });
 
 router.get('/record/:id', function (req, res, next) {
@@ -12,10 +12,16 @@ router.get('/record/:id', function (req, res, next) {
         return res.status(400).send("An id is a required parameter");
     }
     db.read(req.params.id).then(function success(data) {
-        return res.send(data);
+        if(data)
+        {
+            return res.send(data);
+        }
+        else{
+            return res.status(400).send("Record " +  req.params.id + " was not found"); 
+        }
     },
         function error(err) {
-            return res.status(500).send("Something went wrong reading the record: " + req.params.id);
+            return res.status(500).send("Error retrieving record: " + req.params.id);
         });
 });
 
